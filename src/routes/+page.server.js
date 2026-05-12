b063efd3a9b698377f73d4299e6f93c80564e3f4
Sawmii <sawmiya.stanin@gmail.com>
2026-05-12 21:24:58 +0200

    Ansicht angepasst

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,6 +2,7 @@ import db from "$lib/db.js"
 
 export async function load() {
     return {
-        modules: await db.getModules()
+        modules: await db.getModules(),
+        tasks: await db.getTasks()
     };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,35 @@
 <script>
     import ModuleCard from "$lib/components/ModuleCard.svelte";
-    import Tasklist from "$lib/components/TaskList.svelte";
+    import TaskList from "$lib/components/Tasklist.svelte";
     let { data } = $props();
 </script>
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-12 col-md-8 p-5">
+        <div class="col-12 col-lg-8 p-5">
             <h1 class="fw-bold mb-4">Heutige To-Dos</h1>
+
+
+
             <h1 class="fw-bold mb-4">Lernfortschritt</h1>
             {#each data.modules as module}
                 <ModuleCard {module}/>
             {/each}
         </div>
-        <div class="col-12 col-md-4 p-5" style="background-color: #ebebef; min-height: calc(100vh - 94px);">
-            
+
+        <div class="col-12 col-lg-4 p-5 sidebar">
+            <TaskList
+                tasks={data.tasks}
+                modules={data.modules}
+                typ="Termin"
+            />
+            <div class="mt-5 pt-5">
+                <TaskList
+                    tasks={data.tasks}
+                    modules={data.modules}
+                    typ="Prüfungstermin"
+                />
+            </div>
         </div>
     </div>
 </div>
